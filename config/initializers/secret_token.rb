306523132b88fb7c13d1fcd65ab5ef08b69ce2bc
Bojana Jam + Mary Zhu <apprentice+bj+mz@devbootcamp.com>
2012-08-13 12:23:40 -0700

    Added new rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rankforme::Application.config.secret_token = 'c58c7d07037460544745c4ff5544ee3344acc43fef147bebf6a431044bbff4ab383a8d2872a2791c864e7834dd11d89685c8b506969c74545b23f8338f07a6b8'
