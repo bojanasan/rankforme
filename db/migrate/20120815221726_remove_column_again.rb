97e41102a7d8b0db7578db4eada80c4d074f1b08
Bojana Jam + Mary Zhu <apprentice+bj+mz@devbootcamp.com>
2012-08-16 11:11:38 -0700

    Robert & Bojana's changes, yay

--- db/migrate/20120815221726_remove_column_again.rb
+++ db/migrate/20120815221726_remove_column_again.rb
@@ -1,9 +1,9 @@
 class RemoveColumnAgain < ActiveRecord::Migration
   def up
-    remove_column :factors, :user_id
-  end
+       remove_column :factors, :user_id
+     end
 
-  def down
-    add_column :factors, :user_id, :integer
-  end
+   def down
+       add_column :factors, :user_id, :integer
+     end
 end
